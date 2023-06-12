d4c2cb5aaeb209795c3688fe269923b3065db9b5
aetox <core.editor>
2023-06-12 20:26:03 +0200

    modif form

--- src/app/movielist/movielist-add-form/movielist-add-form.component.ts
+++ src/app/movielist/movielist-add-form/movielist-add-form.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl,FormGroup,ReactiveFormsModule } from '@angular/forms';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
 @Component({
   selector: 'app-movielist-add-form',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, CheckboxModule, InputTextModule],
   templateUrl: './movielist-add-form.component.html',
-  styleUrls: ['./movielist-add-form.component.scss']
+  styleUrls: ['./movielist-add-form.component.scss'],
 })
 export class MovielistAddFormComponent {
-
   newGame = new FormGroup({
     title: new FormControl(''),
     img: new FormControl(''),
     description: new FormControl(''),
-    console: new FormControl('')
+    console: new FormControl(''),
   });
-
 }
