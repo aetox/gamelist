3d1ab2560e30471bf2277c95756931533f858935
Aetox <dimitri.kapolas04@gmail.com>
2023-06-12 15:16:31 +0200

    v.1 Form

--- src/app/movielist/movielist-add-form/movielist-add-form.component.ts
+++ src/app/movielist/movielist-add-form/movielist-add-form.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl,FormGroup,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movielist-add-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './movielist-add-form.component.html',
   styleUrls: ['./movielist-add-form.component.scss']
 })
 export class MovielistAddFormComponent {
+
+  newGame = new FormGroup({
+    title: new FormControl(''),
+    img: new FormControl(''),
+    description: new FormControl(''),
+    console: new FormControl('')
+  });
 
 }
